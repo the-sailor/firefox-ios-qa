1281fbbf89f9c305d621f3617d3df7e15d84ea7d
Richard Newman <rnewman@twinql.com>
2015-11-18 20:46:31 -0800

    DEBUG LOGGING

--- Storage/ThirdParty/SwiftData.swift
+++ Storage/ThirdParty/SwiftData.swift
@@ -598,6 +598,7 @@ public class ConcreteSQLiteDBConnection: SQLiteDBConnection {
 
     /// Executes a change on the database.
     public func executeChange(sqlStr: String, withArgs args: [AnyObject?]?) -> NSError? {
+        log.debug("Running change\n \(sqlStr);")
         var error: NSError?
         let statement: SQLiteDBStatement?
         do {
@@ -636,6 +637,7 @@ public class ConcreteSQLiteDBConnection: SQLiteDBConnection {
     /// Queries the database.
     /// Returns a cursor pre-filled with the complete result set.
     func executeQuery<T>(sqlStr: String, factory: ((SDRow) -> T), withArgs args: [AnyObject?]?) -> Cursor<T> {
+        log.debug("Running query \(sqlStr);")
         var error: NSError?
         let statement: SQLiteDBStatement?
         do {
