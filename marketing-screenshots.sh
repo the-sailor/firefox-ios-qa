cc34fadfb1d686b7f4376058a4107f35a85ae1ac
Stefan Arentz <stefan@arentz.ca>
2016-04-20 15:18:15 -0400

    WIP

--- marketing-screenshots.sh
+++ marketing-screenshots.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+LANGUAGES="en-US de-DE fr-FR"
+DEVICES="iPhone 4s,iPhone 5s,iPhone 6s,iPhone 6s Plus,iPad Air,iPad Pro"
+DEVICES="iPhone 4s,iPhone 5s,iPhone 6s,iPhone 6s Plus"
+SNAPSHOT=/Users/sarentz/Projects/fastlane/snapshot/bin/snapshot
+
+LANGUAGES="fr-FR"
+DEVICES="iPhone 4s"
+
 if [ ! -d firefox-ios-l10n ]; then
     echo "Did not find a firefox-ios-l10n checkout. Are you running this on a localized build?"
     exit 1
@@ -12,23 +20,15 @@ fi
 
 mkdir marketing-screenshots
 
-DEVICES="iPhone 4s,iPhone 5s,iPhone 6s,iPhone 6s Plus,iPad Air,iPad Pro"
-LANGUAGES="en-US,de,fr,es"
-
-DEVICES="iPhone 6s,iPad Air"
-LANGUAGES="en-US,de,es,fr"
-
-#LANGUAGES="af,ar,az,bg,bn-BD,bn-IN,br,bs,cs,cy,da,de,dsb,el,en-US,eo,es,es-CL,es-MX,fa,fr,fy-NL,ga-IE,gd,gl,hsb,id,is,it,ja,kk,km,kn,ko,lo,lt,lv,ml,ms,my,nb-NO,ne-NP,nl,nn-NO,or,pl,pt-BR,pt-PT,rm,ro,ru,ses,sk,sl,sv-SE,th,tl,tn,tr,uk,uz,zh-CN,zh-TW"
-
-SNAPSHOT=/Users/sarentz/Projects/fastlane/snapshot/bin/snapshot
-#SNAPSHOT=snapshot
-
-echo "`date` Snapshotting $lang"
-$SNAPSHOT --project Client.xcodeproj --scheme MarketingUITests \
+for lang in $LANGUAGES; do
+  echo "`date` Snapshotting $lang"
+  mkdir marketing-screenshots/$lang
+  $SNAPSHOT --project Client.xcodeproj --scheme MarketingUITests \
     --derived_data_path marketing-screenshots-dd \
     --erase_simulator \
     --localize_simulator \
     --devices "$DEVICES" \
-    --languages "$LANGUAGES" \
-    --output_directory marketing-screenshots # > marketing-screenshots.log 2>&1
+    --languages $lang \
+    --output_directory marketing-screenshots/$lang # > marketing-screenshots.log 2>&1
+done
 
