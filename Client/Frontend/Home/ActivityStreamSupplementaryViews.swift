6e113637feaf9e622a702b2419895eb0cd71572d
farhanpatel <farhanpatel.17@gmail.com>
2016-08-04 19:35:01 -0700

    A paging topsites.

--- Client/Frontend/Home/ActivityStreamSupplementaryViews.swift
+++ Client/Frontend/Home/ActivityStreamSupplementaryViews.swift
@@ -75,6 +75,86 @@ class TopSiteCell: UICollectionViewCell {
     
 }
 
+class ASVerticalScrollCell: UICollectionViewCell {
+    var collectionView: UICollectionView!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        let layout  = UICollectionViewFlowLayout()
+        layout.scrollDirection = UICollectionViewScrollDirection.Horizontal
+        layout.itemSize = CGSize(width: 100, height: 100)
+
+        collectionView = UICollectionView(frame: CGRectZero, collectionViewLayout: layout)
+        collectionView.registerClass(TopSiteCell.self, forCellWithReuseIdentifier: "TopSiteCell")
+        collectionView.backgroundColor = UIColor.whiteColor()
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.pagingEnabled = true
+
+        addSubview(collectionView)
+        collectionView.snp_makeConstraints { make in
+            make.edges.equalTo(self)
+        }
+    }
+
+
+    func setDelegate(delegate: ASVerticalScrollSource) {
+        collectionView.delegate = delegate
+        collectionView.dataSource = delegate
+        collectionView.reloadData()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+//both should conform to a protocol. Where the title is given. and the image is given? and other basic stuff is given
+//this will allow both to work in the ASVerticalScrollSource
+struct TopSiteItem {
+    let urlTitle: String
+    let faviconURL: NSURL
+    let backgroundColor: UIColor
+    let textColor: UIColor
+    let size: CGSize
+}
+
+struct ASAction {
+    let title: String
+    let image: UIImage
+}
+
+class ASVerticalScrollSource: NSObject, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
+    var content: [TopSiteItem] =  []
+
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return content.count
+    }
+
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        let contentItem = content[indexPath.row]
+        return contentItem.size
+    }
+
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("TopSiteCell", forIndexPath: indexPath) as! TopSiteCell
+        //go through content and set stuff based on type of the struct provided
+        let contentItem = content[indexPath.row]
+        cell.titleLabel.text = contentItem.urlTitle
+        cell.setImageWithURL(contentItem.faviconURL)
+//        cell.backgroundColor = contentItem.backgroundColor
+//        cell.titleLabel.textColor = contentItem.textColor
+        return cell
+    }
+
+//    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat {
+//        return 22
+//    }
+
+
+}
+
+
 class ActivityStreamHeaderView: UICollectionReusableView {
     var titleLabel: UILabel!
     var moreLabel: UIButton!
